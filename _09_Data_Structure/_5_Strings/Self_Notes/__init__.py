8a88eb1863ad4e64655066a7859b089424a0158f
shettymodernchipsolutions <91957357+shettymodernchipsolutions@users.noreply.github.com>
2021-12-28 09:00:58 +0530

    Commit

--- _09_Data_Structure/_5_Strings/Self_Notes/__init__.py
+++ _09_Data_Structure/_5_Strings/Self_Notes/__init__.py
@@ -14,8 +14,8 @@
 
 # s = 'Practice makes Perfect'
 # print(s)
-#s = 'Practice makes 'Perfect''
-#print(s)
+# s = 'Practice makes 'Perfect''
+# print(s)
 #
 # s = 'Practice makes "Perfect"'
 # print(s)
@@ -235,10 +235,402 @@
 # else:
 #     print("String values are not equal")
 
-s1 = "python"
-s2 = "PYTHON"
+# s1 = "python"
+# s2 = "PYTHON"
+#
+# if s1.upper() == s2.upper():
+#     print("String values are equal.")
+# else:
+#     print("String values are not equal")
+#
+# char = 'Bhavesh V Shetty'
+#
+# n = len(char)
+#
+# i = 0
+#
+# while i < n:
+#     print(char[i], end=' ')
+#     i += 1
+#
+# print()
+#
+# i = -1
+#
+# while i >= -n:
+#     print(char[i], end=' ')
+#     i -= 1
+#
+# print()
+#
+# i = 1
+#
+# while i <= n:
+#     print(char[-i], end=' ')
+#     i += 1
+#
+# st = 'Core Python'
+#
+# t = len(st)
+#
+# i = 0
+#
+# while i < t:
+#     print(st[i], end=' ')
+#     i += 1
+#
+# print()
+#
+# i = -1
+#
+# while i >= -t:
+#     print(st[i], end=' ')
+#     i -= 1
+#
+# print()
+#
+# i = 1
+#
+# while i <= t:
+#     print(st[-i], end=' ')
+#     i += 1
 
-if s1.upper() == s2.upper():
-    print("String values are equal.")
-else:
-    print("String values are not equal")
+# # A program to access each element of a string in forward and reverse orders using while loop
+# char = 'Bhavesh V Shetty'
+#
+# n = len(char)
+#
+# i = 0
+#
+# while i < n:
+#     print(char[i], end=' ')
+#     i += 1
+#
+# print()
+#
+# i = -1
+#
+# while i >= -n:
+#     print(char[i], end=' ')
+#     i -= 1
+#
+# print()
+#
+# i = 1
+#
+# while i <= n:
+#     print(char[-i], end=' ')
+#     i += 1
+
+
+# A program to access character of a string using for loop
+
+# str = 'Core Python'
+#
+# for i in str:
+#     print(i, end=' ')
+#
+# print()
+#
+# for i in str[::-1]:
+#     print(i, end=' ')
+
+
+# # A python program to know whether a sub string exists in main string or not
+#
+# char = input("Enter the main string: ")
+# sub = input("Enter the sub string: ")
+# if sub in char:
+#     print(sub + ' is found in main string')
+# else:
+#     print(sub + ' not found in main string')
+
+
+# char = 'Core Python'
+#
+# n = len(char)
+#
+# i = 0
+#
+# while i < n:
+#     print(char[i], end=' ')
+#     i += 1
+#
+# print()
+#
+# i = -1
+#
+# while i >= -n:
+#     print(char[i], end=' ')
+#     i -= 1
+#
+# i = 1
+#
+# print()
+#
+# while i <= n:
+#     print(char[-i], end=' ')
+#     i += 1
+
+
+# chr = 'Core Python'
+#
+# for i in chr:
+#     print(i, end=' ')
+#
+# print()
+#
+# for i in chr[::-1]:
+#     print(i, end=' ')
+#
+# print()
+#
+# char = input("Enter the string: ")
+# sub = input("Enter the string: ")
+#
+# if sub in char:
+#     print(sub + ' is present in char')
+# else:
+#     print(sub + ' is not present in char')
+
+
+# A program to find the first occurence of sub string in a given main string.
+
+# char = input("Enter the string: ")
+# chr = input("Enter the string: ")
+#
+# n = char.find(chr, 0, len(char))
+#
+# if n == -1:
+#     print('Sub string is not found')
+# else:
+#     print('Sub string is found at position', n + 1)
+#
+# stat = input('Enter the string: ')
+# ghy = input("Enter the string: ")
+#
+# n = stat.find(ghy, 0, len(stat))
+#
+# if n == -1:
+#     print("Sub string is not found")
+# else:
+#     print("Sub sting is found at position", n + 1)
+
+
+# A Python program to find the first occurence of sub string in a given main string using index() method.
+#
+# char = input("Enter the string: ")
+# chr = input("Enter the string: ")
+#
+# try:
+#     n = char.index(chr, 0, len(char))
+# except ValueError:
+#     print("Entered string is not found")
+# else:
+#     print("Sub string is found at position ", n + 1)
+#
+
+
+# char = input("Enter the string: ")
+# chr = input("Enter the sub string: ")
+#
+# try:
+#     n = char.index(chr, 0, len(char))
+# except ValueError:
+#     print("Sub string is not found")
+# else:
+#     print("Sub string is found at position", n + 1)
+
+
+# A Python program to display all positions of a sub string in a given main string.
+
+# char = input("Enter the string: ")
+# chr = input("Enter the sub string : ")
+#
+# i = 0
+# flag = False
+# n = len(char)
+#
+# while i < n:
+#     pos = char.find(chr, i, n)
+#     if pos != -1:
+#         print("Sub string found at position: ", pos + 1)
+#         i = pos + 1
+#         flag = True
+#     else:
+#         i += 1
+#
+# if flag == False:
+#     print("Sub string is not found")
+
+
+# char = 'Core Python'
+#
+# n = len(char)
+#
+# i = 0
+#
+# while i < n:
+#     print(char[i], end=' ')
+#     i += 1
+#
+# print()
+#
+# i = -1
+#
+# while i >= -n:
+#     print(char[i], end=' ')
+#     i -= 1
+#
+# print()
+#
+# i = 1
+#
+# while i <= n:
+#     print(char[-i], end=' ')
+#     i += 1
+#
+# print()
+#
+# char = 'Core Python'
+#
+# for i in char:
+#     print(i, end=' ')
+#
+# print()
+#
+# for i in char[::-1]:
+#     print(i, end=' ')
+#
+# print()
+#
+# str = input("Enter the string: ")
+# sub = input("Enter the string: ")
+#
+# if sub in str:
+#     print(sub + ' string found in str')
+# else:
+#     print(sub + ' string not found in str')
+#
+# print()
+#
+# char = input('Enter a string: ')
+# chr = input('Enter a sub string: ')
+#
+# n = char.find(chr, 0, len(char))
+#
+# if n == -1:
+#     print("Sub string not found")
+# else:
+#     print("Sub string found at position ", n + 1)
+
+
+# char = input("Enter a string: ")
+# chr = input("Enter a string: ")
+#
+# try:
+#     n = char.index(chr, 0, len(char))
+# except ValueError:
+#     print("Sub string not found")
+# else:
+#     print("Sub string found at position", n + 1)
+#
+# char = input("Enter a string: ")
+# chr = input("Enter a sub string: ")
+#
+# n = len(char)
+# flag = False
+# i = 0
+#
+# while i < n:
+#     pos = char.find(chr, i, n)
+#     if pos != -1:
+#         print('Sub string found at position: ', pos + 1)
+#         i = pos + 1
+#         flag = True
+#     else:
+#         i += 1
+#
+# if flag == False:
+#     print('Sub string not found')
+#
+
+
+# char = input("Enter a string: ")
+# chr = input("Enter a sub string: ")
+#
+# flag = False
+# i = 0
+# n = len(char)
+#
+# while i < n:
+#     pos = char.find(chr, i, n)
+#     if pos != -1:
+#         print("Sub string found at position ", pos + 1)
+#         i = pos + 1
+#         flag = True
+#     else:
+#         i += 1
+# if flag == False:
+#     print("Sub string not found")
+
+# A Python program to display all positions of a sub string in a given main string.
+
+# char = input("Enter a string: ")
+# chr = input("Enter a sub string: ")
+#
+# i = 0
+# flag = False
+# n = len(char)
+#
+# while True:
+#     pos = char.find(chr, pos + 1, n)
+#     if pos == -1:
+#         break
+#     print('Sub string found at position: ', pos + 1)
+#     flag = True
+#
+# if flag == False:
+#     print('Sub string not found')
+#
+# # A Python program to find all the positions of a sub string in a given main string.
+#
+# char = input("Enter a string: ")
+# chr = input("Enter the sub string: ")
+#
+# i = 0
+# n = len(char)
+# flag = False
+#
+# while True:
+#     pos = char.find(chr, pos + 1, n)
+#     if pos == -1:
+#         break
+#     print('Sub string found at position: ', pos + 1)
+#     flag = True
+#
+# if flag == False:
+#     print("Sub string not found")
+
+
+
+# s = '     I am a Python Developer       '
+# print(s.lstrip())
+# print(s.rstrip())
+# print(s.strip())
+
+
+# A Python program to accept and display a group of numbers.
+
+# char = ['10', '20', '30', '40', '50']
+#
+# str = ':'
+#
+# char1 = str.join(char)
+#
+# print(char1)
+
+str = 'Python is the future'
+
+str = str.upper()
+str = str.lower()
---
+++ _22_Multi_Threading/Self_Notes/__init__.py
@@ -0,0 +1,217 @@
+# A Python program to find the currently running thread in a Python program
+
+import threading
+print('Let us find the current thread')
+
+print('Currently running thread: ', threading.current_thread().getName())
+
+if threading.current_thread() == threading.main_thread():
+    print('The current thread is the main thread')
+else:
+    print('The current thread is not main thread')
+
+
+# A Python program to create a thread and use it to run a function.
+
+from threading import *
+
+def display():
+    print('Hello I am running')
+
+for i in range(5):
+    t = Thread(target=display)
+
+    t.start()
+
+
+# A Python program to pass arguments to a function and execute it using a thread.
+
+from threading import *
+
+def display(str):
+    print(str)
+
+for i in range(5):
+    t = Thread(target=display, args=('Hello', ))
+    t.start()
+
+
+# A Python program to create a thread by making our class as sub class to Thread class
+
+from threading import Thread
+
+class MyThread(Thread):
+    def run(self):
+        for i in range(1, 6):
+            print(i)
+
+t1 = MyThread()
+
+t1.start()
+
+t1.join()
+
+
+# A Python program to create a thread that accesses the instance variables of a class.
+
+from threading import *
+
+class MyThread(Thread):
+
+    def __init__(self, str):
+        Thread.__init__(self)
+        self.str = str
+
+    def run(self):
+        print(self.str)
+
+t1 = MyThread('Hello')
+
+t1.start()
+
+t1.join()
+
+
+# A Python program to create a thread that acts on the object of a class that is not derived from a Thread class.
+
+from threading import *
+
+class MyThread:
+
+    def __init__(self, str):
+        self.str = str
+
+    def display(self, x, y):
+        print(self.str)
+        print('The args are: ', x, y)
+
+obj = MyThread('Hello')
+
+t1 = Thread(target=obj.display, args=(1, 2))
+
+t1.start()
+
+
+# A Python program to show single tasking using a thread that prepares tea.
+
+from threading import *
+from time import *
+
+class MyThread:
+    def prepareTea(self):
+        self.task1()
+        self.task2()
+        self.task3()
+
+    def task1(self):
+        print('Boil milk and tea powder for 5 minutes....', end='')
+        sleep(5)
+        print('Done')
+
+    def task2(self):
+        print('Add sugar and boil for 3 minutes....', end='')
+        sleep(3)
+        print('Done')
+
+    def task3(self):
+        print('Filter it and serve....', end='')
+        print('Done')
+
+obj = MyThread()
+
+t = Thread(target=obj.prepareTea)
+t.start()
+
+
+# A Python program that performs two tasks using two threads simultaneously.
+
+from threading import *
+from time import *
+
+class Theatre:
+
+    def __init__(self, str):
+        self.str = str
+
+    def movieshow(self):
+        for i in range(1, 6):
+            print(self.str, " : ", i)
+            sleep(0.1)
+
+obj1 = Theatre('Cut ticket')
+obj2 = Theatre('Show chair')
+
+t1 = Thread(target=obj1.movieshow)
+t2 = Thread(target=obj2.movieshow)
+
+t1.start()
+t2.start()
+
+
+# A program where two threads are acting on the same method to allot a berth for the passenger.
+
+from threading import *
+from time import *
+
+class Railway:
+
+    def __init__(self, available):
+        self.available = available
+
+    def reserve(self, wanted):
+        print('Available number of berths: ', self.available)
+
+        if(self.available >= wanted):
+            name = current_thread().getName()
+            print('%d berths alloted for %s' %(wanted, name))
+            sleep(1.5)
+            self.available -= wanted
+        else:
+            print('Sorry, no berths to allot')
+
+obj = Railway(1)
+
+t1 = Thread(target=obj.reserve, args=(1,))
+t2 = Thread(target=obj.reserve, args=(1,))
+
+t1.setName('First Person')
+t2.setName('Second Person')
+
+t1.start()
+t2.start()
+
+
+# A Python program for achieving thread synchronization using locks.
+
+from threading import *
+from time import *
+
+class Railway:
+
+    def __init__(self, available):
+        self.available = available
+        self.l = Lock()
+
+    def reserve(self, wanted):
+        self.l.acquire()
+        print('Available number of berths: ', self.available)
+
+        if(self.available >= wanted):
+            name = current_thread().getName()
+            print('%d berths alloted for %s' %(wanted, name))
+            sleep(1.5)
+            self.available -= wanted
+        else:
+            print('Sorry, no berths to allot')
+        self.l.release()
+
+obj = Railway(1)
+
+t1 = Thread(target=obj.reserve, args=(1,))
+t2 = Thread(target=obj.reserve, args=(1,))
+
+t1.setName('First Person')
+t2.setName('Second Person')
+
+t1.start()
+t2.start()
